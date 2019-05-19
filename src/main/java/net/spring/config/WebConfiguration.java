2d322f0b4d9080388536d4cd5a99228ca2e610e4
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-05-19 12:38:42 +0300

    fixed some comments

--- src/main/java/net/spring/config/WebConfiguration.java
+++ src/main/java/net/spring/config/WebConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 public class WebConfiguration implements WebMvcConfigurer {
     private final StudentInterceptor studentInterceptor;
 
