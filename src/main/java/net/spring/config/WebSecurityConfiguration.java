418d9a033c793de145bd90f3b86959d48869030b
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-05-23 17:15:21 +0300

    security finished

--- src/main/java/net/spring/config/WebSecurityConfiguration.java
+++ src/main/java/net/spring/config/WebSecurityConfiguration.java
@@ -59,6 +59,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**");
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**");
     }
 }
